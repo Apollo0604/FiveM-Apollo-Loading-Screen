73cd9eb88c4c807636a4ad8702959c3a5165728d
Jeremiah (Jay) <58581408+Apollo0604@users.noreply.github.com>
2020-04-07 15:08:55 -0500

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+-- Server needs to know what to read.
+files {
+    "index.html",
+    "style/style.css"
+}
+
+
+-- What .html files is being used in loadinscreen.
+loadingscreen {
+    "index.html"
+}
+
+-- FiveM Shit.
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
